69d79e176e49fb5728a906ec0a6eda92ae35c936
Austin Ah Loo <austin.hka@gmail.com>
2025-09-05 09:44:03 -0700

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Austin Ah Loo on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
